b5010d4d17cd741e8a6b796e983554474f16d126
Omar Amr <omaradel9545@gmail.com>
2017-11-12 04:53:59 +0100

    [ADD] Testing Directories + EX1 Solution

---
+++ ex1.py
@@ -0,0 +1,22 @@
+import os
+
+
+def scan_directory(directory):
+    files_counter = 0
+    found_files = []
+    for folder in os.walk(os.path.abspath(os.path.expanduser(directory))):
+        for file in folder[2]:
+            if file.endswith(".exp1.data"):
+                file_full_path = folder[0] + "/" + file
+                print(file_full_path)
+                found_files.append(file_full_path)
+                files_counter += 1
+
+    print("Number of files: " + str(files_counter))
+    return found_files
+
+
+directory1 = "~/Desktop/Special-Topics-on-Bioinformatics-Project/Test Folder"
+directory2 = "/Users/omaramr/Desktop/Special-Topics-on-Bioinformatics-Project/Test Folder"
+
+file_names_list = scan_directory(directory2)
