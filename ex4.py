00044574160f796609b23472f39de9a9a2be98e6
Omar Amr <omaradel9545@gmail.com>
2017-11-25 23:02:51 +0100

    [EDIT] small modifications to histogram graph

--- ex4.py
+++ ex4.py
@@ -5,7 +5,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 from subprocess import call
 
-
 arg1 = sys.argv[1]
 
 
@@ -17,13 +16,17 @@ def shell_script(directory=arg1):
     heights = pickle.load(open(output_directory + "heights.pkl", "rb"))
 
     bin_upper_bound = math.ceil(np.amax(heights)) + 1
-    n_bins = range(0, bin_upper_bound)
+    n_bins = np.arange(0.0, bin_upper_bound, 0.1)
 
-    arr = plt.hist(heights, n_bins, histtype='bar', edgecolor='white', color="red")
+    plt.figure(figsize=(25, 15))
 
-    for i in range(0, bin_upper_bound - 1):
+    arr = plt.hist(heights, n_bins, histtype='bar', color="red")
+    plt.tight_layout(pad=2)
+
+    for i in range(0, arr[0].size):
         if arr[0][i] != 0:
-            plt.text(arr[1][i], arr[0][i], str(int(arr[0][i])), rotation=90)
+            # bin_label = "Count({}) = {}".format(str(float(arr[1][i])), str(int(arr[0][i])))
+            plt.text(arr[1][i], arr[0][i], str(int(arr[0][i])), rotation=0)
 
     plt.title("Heights")
     plt.savefig(output_directory + "/heights.png")
