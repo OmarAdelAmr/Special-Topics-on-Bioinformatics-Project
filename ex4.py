dad2e0f5a75387c7929900804f0bf9de73f15d77
Omar Amr <omaradel9545@gmail.com>
2017-11-25 23:33:29 +0100

    [EDIT] histogram graph modifications -> x axis numbers and font size above bars

--- ex4.py
+++ ex4.py
@@ -18,15 +18,17 @@ def shell_script(directory=arg1):
     bin_upper_bound = math.ceil(np.amax(heights)) + 1
     n_bins = np.arange(0.0, bin_upper_bound, 0.1)
 
-    plt.figure(figsize=(25, 15))
-
+    fig = plt.figure(figsize=(25, 15))
+    ax = fig.add_subplot(111)
+    ax.set_xticks(np.arange(len(n_bins)))
     arr = plt.hist(heights, n_bins, histtype='bar', color="red")
     plt.tight_layout(pad=2)
 
     for i in range(0, arr[0].size):
         if arr[0][i] != 0:
+            # TODO: Add height value on graph for each bar. Next Line
             # bin_label = "Count({}) = {}".format(str(float(arr[1][i])), str(int(arr[0][i])))
-            plt.text(arr[1][i], arr[0][i], str(int(arr[0][i])), rotation=0)
+            plt.text(arr[1][i], arr[0][i], str(int(arr[0][i])), rotation=0, fontsize=15)
 
     plt.title("Heights")
     plt.savefig(output_directory + "/heights.png")
